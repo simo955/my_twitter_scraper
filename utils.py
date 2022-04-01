a0a1cbc71b72dc26f05816795df2814b21a308a8
simo955 <simone.patuelli@icloud.com>
2022-04-01 19:04:11 +0200

    First streaming-crawling done!

--- utils.py
+++ utils.py
@@ -1,22 +1,31 @@
 import pandas as pd
 
+WANTED_FIELDS=['id','text', 'author_id', 'created_at', 'public_metrics', 'lang']
+
+
 def save_to_df(results, df):
-    # TODO: if df is None: crea df
+    if df is None:
+        df = pd.DataFrame([], columns=WANTED_FIELDS)
     formatted_results = []
     for elem in results:
         formatted_results.append( \
-            {'text': elem['text'], \
+            {'id': elem['id'], \
+            'text': elem['text'], \
             'author_id': elem['author_id'], \
-            'created_at': elem['created_at'] \
+            'created_at': elem['created_at'], \
+            'public_metrics': elem['public_metrics'], \
+            'lang': elem['lang'] \
             })
 
+    print('DF UPDATED')
     return df.append(formatted_results)
 
 def save_df_to_csv(df, path, index=False):
     try:
         df.to_csv(path, index=index)
+        print('DF Saved on file {}'.format(path))
     except Exception as e:
-        print(e, 'Something went wrong while saving')
+        print(e, 'Something went wrong while saving the DF on file.')
 
 
 # df = pd.DataFrame([['text1', 'author1', 'created at 1'], ['text2', 'author1', 'created at 2']], columns=['text', 'author_id', 'created_at'])
