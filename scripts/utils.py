d619b2accedc5b31ad618d9108f808f6c1225b7f
patu955 <simone.patuelli@icloud.com>
2022-05-21 18:53:16 +0200

    Added moving script

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,53 @@
-import pandas as pd
-import numpy as np
+import os
 import json
+import shutil
+import pandas as pd
+
+data_path='destination'
+
+def unpack_downloaded_data():
+    dirs = [d for d in os.listdir() if '.' not in d]
+    dest_dir='destination/'
+
+    try: 
+        os.mkdir(dest_dir) 
+        print('Created destination repo {}!'.format(dest_dir))
+    except OSError as error: 
+        print(error)
 
 
-def create_compounded_csv(num_csv=10, basic_path='../data'):
+    for d in dirs:
+        error_flag=0
+        files = [f.split('.csv')[0] for f in os.listdir(d) if '.csv' in f]
+    
+        print('In directory {} found files: {}'.format(d, files))
+        for f in files:
+            original_path = d+'/'+f+'.csv'
+            new_path = dest_dir+f+'_'+d+'.csv'
+            try:
+                #Move file to new path
+                os.rename(original_path, new_path)
+            except Exception as e:
+                print('Unable to move file. Exception', e)
+                error_flag=1
+        
+        if error_flag==0:
+        #Delete repository
+            try:
+                print('Removing {} directory'.format(d))
+                shutil.rmtree(d)
+            except OSError as e:
+                print("Error: %s : %s" % (d, e.strerror))
+
+def create_compounded_csv(files):
     compounded_df = pd.DataFrame(columns=['id','text','author_id','created_at','public_metrics','lang','geo'])
-
-    for i in range(0, num_csv+1):
-        pathname = basic_path+'/tweets-{}.csv'.format(i)
+    
+    for path in files:
         try:
-            dt = pd.read_csv(pathname, dtype={'id':str,'text':str, 'author_id':str})
+            dt = pd.read_csv(data_path+'/'+path, dtype={'id':str,'text':str, 'author_id':str})
+
         except:
-            print('Unable to read:', pathname)
+            print('Unable to read:', path)
             continue
         compounded_df = pd.concat([dt,compounded_df] , ignore_index=True)
         print('New df is now long: {}'.format(compounded_df.shape[0]))
@@ -23,7 +59,6 @@ def create_compounded_csv(num_csv=10, basic_path='../data'):
 
     return compounded_df
 
-
 def create_dummy_columns(dt, column_name='public_metrics'):
     dicts=[json.loads(x) for x in dt[column_name].values]
     dt['retweet_count']=[x['retweet_count'] for x in dicts]
@@ -32,13 +67,11 @@ def create_dummy_columns(dt, column_name='public_metrics'):
     dt['quote_count']=[x['quote_count'] for x in dicts]
     return dt.drop(columns=['public_metrics'])
 
-
 if __name__ == "__main__":
-    dt1 = create_compounded_csv(num_csv=38,basic_path='../data')
-    dt2 = create_compounded_csv(num_csv=38,basic_path='../data1')
-    dt3 = create_compounded_csv(num_csv=38,basic_path='../data2')
+    files =  [f for f in os.listdir(data_path) if '.csv' in f]
 
-    dt = pd.concat([dt1, dt2, dt3], ignore_index=True)
+    dt = create_compounded_csv(files)
+
     dt=dt.drop_duplicates(subset=['id'], keep='first')
     dt=create_dummy_columns(dt)
 
